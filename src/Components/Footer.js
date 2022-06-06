6647d752ada89a947e8375af7d4ecac20bb1dfc1
ninadsutrave <ninadsutrave@gmail.com>
2022-06-07 01:19:34 +0530

    security concerns fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className="Footer">
-        Created by <a href="https://github.com/NinadSutrave" target="_blank"> Ninad Sutrave </a>
+        Created by <a href="https://github.com/NinadSutrave" target="_blank" rel="noreferrer"> Ninad Sutrave </a>
     </div>
   )
 }
