6b9355345c3f8ad7324387946a19ee0b8918e122
ninadsutrave <ninadsutrave@gmail.com>
2022-06-11 14:52:56 +0530

    error route corrected

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -10,7 +10,7 @@ function Error() {
         <h1>Page not found!</h1>
         <img className="ErrImage" alt="Error 404" src={Error404} />
         <h4 className="error-message">The URL you are looking for does not exist</h4>
-        <h4 className="error-message">Go back to <a href="http://localhost:3000/">homepage</a>?</h4> 
+        <h4 className="error-message">Go back to <a href={process.env.REACT_APP_FRONT_URL}>homepage</a>?</h4> 
     </div>
   )
 }
